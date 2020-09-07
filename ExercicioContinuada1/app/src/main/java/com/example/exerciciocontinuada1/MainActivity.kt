be22321bc3e35d24f0a0f29b99659517abc9c027
unknown <giovanna.ab.souza10@gmail.com>
2020-09-07 14:41:07 -0300

    v2

--- ExercicioContinuada1/app/src/main/java/com/example/exerciciocontinuada1/MainActivity.kt
+++ ExercicioContinuada1/app/src/main/java/com/example/exerciciocontinuada1/MainActivity.kt
@@ -12,47 +12,80 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    //Toast.makeText(
+    // this,
+    // "Um nome possui pelo menos 3 letras, #NãoQueroApelidos",
+    // Toast.LENGTH_SHORT
+    // ).show()
 
-    fun botao(componente:View){     
+   // Toast.makeText(
+    //this,
+   // "Você só pode ser um fantasma, digite um ano com mais de 4 digitos",
+   // Toast.LENGTH_SHORT
+    //).show()
 
-        val idade = et_idade.text.toString().toInt()
-        val ano = et_ano.text.toString().toInt()
+    //Toast.makeText(
+    //this,
+    //"Você é Abraão? Por favor digite sua idade novamente..",
+    //Toast.LENGTH_SHORT
+    //).show()
+
+    fun validaName(): Boolean {
         val nome = et_nome.text.toString().toString()
+        var correto = true
 
-        val resultado = ano - idade
-
-        if (et_nome.length() < 2){
-            Toast.makeText(
-                this,
-                "Um nome possui pelo menos 3 letras, #NãoQueroApelidos",
-                Toast.LENGTH_SHORT
-            ).show()
+        if (et_nome.length() <= 2) {
+            et_nome.error = "Um nome possui pelo menos 3 letras, #NãoQueroApelidos"
+            correto = false
+            return correto
         }
+        return correto
+    }
 
-        if (et_ano.length() < 3 || et_ano.length() > 4) {
-            Toast.makeText(
-                this,
-                "Você só pode ser um fantasma, digite um ano com mais de 4 digitos",
-                Toast.LENGTH_SHORT
-            ).show()
+    fun validarAno(): Boolean{
+        val ano = et_ano.text.toString().toInt()
+        var correto = true
+
+        if (et_ano.length() <= 3 || et_ano.length() > 4) {
+           et_ano.error ="Você só pode ser um fantasma, digite um ano com mais de 4 digitos"
+            correto = false
+            return correto
         }
+        return correto
+    }
 
-        if (et_idade.length() < 0 || et_idade.length() > 300)
-            Toast.makeText(
-                this,
-                "Ou você é um bb na barriga da mamãe, ou você é Abraão! Por favor digite sua idade novamente..",
-                Toast.LENGTH_SHORT
-            ).show()
+    fun validarIdade() :Boolean{
+        val idade = et_idade.text.toString().toInt()
+        var correto = true
 
-        if (resultado <= 1999){
-            tv_resultado.text = "$nome o ano que você nasceu foi $resultado . TEMPO DA PEDRA!"
-            tv_resultado.setTextColor(Color.GRAY)
-
-        }else if(resultado > 2000){
-            tv_resultado.text = "$nome você nasceu no ano $resultado . ERA DA TECNOLOGIA!"
-            tv_resultado.setTextColor(Color.BLUE)
+        if(et_idade.length() >= 4){
+            et_idade.error = "Você é Abraão? Por favor digite sua idade novamente.."
+            correto = false
+            return correto
         }
+        return correto
+    }
+
+
+    fun botao(componente:View) {
+
+        if (validaName() || validarAno() || validarIdade()) {
+            val idade = et_idade.text.toString().toInt()
+            val ano = et_ano.text.toString().toInt()
+            val nome = et_nome.text.toString().toString()
+
+            val resultado = ano - idade
+
+            if (resultado <= 1999) {
+                tv_resultado.text = "$nome o ano que você nasceu foi $resultado . TEMPO DA PEDRA!"
+                tv_resultado.setTextColor(Color.GRAY)
+
+            } else if (resultado > 2000) {
+                tv_resultado.text = "$nome você nasceu no ano $resultado . ERA DA TECNOLOGIA!"
+                tv_resultado.setTextColor(Color.BLUE)
+            }
 
         }
+    }
 
 }
